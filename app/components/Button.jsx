876e77dfd6d206805fed4f3e6f8999e01f80e3f3
Kanish Kumar <kanishkumar11@outlook.in>
2023-10-05 00:35:31 +0530

    button

--- app/components/Button.jsx
+++ app/components/Button.jsx
@@ -10,7 +10,7 @@ const Button = (props) => {
       href="https://docs.google.com/forms/d/e/1FAIpQLScJcjgnCsFR9zcYXF_HjWJzcfu_KDqOJZjsI96TYtp3U3Xmig/viewform?embedded=true"
     >
       <button
-        className={`${poppins.className} rounded-[8px] bg-[#7c0d60] px-8 py-2 12l:px-10 lg:px-6 text-white hover:scale-105 transition-transform ease-in-out 12l:text-base lg:text-base cursor-pointer`}
+        className={`${poppins.className} rounded-[8px] bg-[#7c0d60] px-8 py-4 lg:py-2 12l:px-10 lg:px-6 text-white hover:scale-105 transition-transform ease-in-out 12l:text-base lg:text-base cursor-pointer`}
       >
         join Whishlist
       </button>
